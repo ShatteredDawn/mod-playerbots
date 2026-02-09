10dc649016f93d8de4fd9d56462c798a3ce042d6
SmashingQuasar <nicolas.cordier@outlook.com>
2026-02-09 22:52:24 +0100

    chore: Added a Clang guard for a Clang pragma.

--- src/Bot/Engine/NamedObjectContext.h
+++ src/Bot/Engine/NamedObjectContext.h
@@ -172,12 +172,16 @@ public:
             if (i->second)
             {
                 // @TODO: This needs to be addressed by refactoring those raw pointers to std::unique_ptr or values.
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdelete-incomplete"
+#if defined(__clang__)
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdelete-incomplete"
+#endif
 
                 delete i->second;
 
-#pragma clang diagnostic pop
+#if defined(__clang__)
+  #pragma clang diagnostic pop
+#endif
             }
         }
 
