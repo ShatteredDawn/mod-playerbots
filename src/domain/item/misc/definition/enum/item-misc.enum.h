eb28074e8d266b8654257ee343a304298fb82ff3
SmashingQuasar <nicolas.cordier@outlook.com>
2026-03-08 13:01:01 +0100

    feat: Added miscellaneous items.

---
+++ src/domain/item/misc/definition/enum/item-misc.enum.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+
+enum class ItemMiscEnum : uint32_t
+{
+
+};
---
+++ src/domain/item/reagent/reagent/definition/enum/item-reagent-reagent.enum.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+enum class ItemReagentReagentEnum : uint32_t
+{
+    FLASH_POWDER = 5140,
+    WORTHLESS_BLINDING_POWDER = 5530,
+    ANKH = 17030,
+    NPC_EQUIP_22453 = 22453,
+    NPC_EQUIP_22454 = 22454,
+    NPC_EQUIP_22455 = 22455,
+};
