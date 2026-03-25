shenkai4257/mod-playerbots-liyunfan

Restore Naxx Strategies without core dependencies (#2031)
### Summary
This PR restores the Naxxramas raid strategies that were removed in
commit 686fe513b25bbb20ccfcc89f08ee8c4b498a263e .
The reintroduced logic is core‑friendly (no AzerothCore script headers
or internal boss AI/EventMap dependencies), and the Naxxramas actions
have been refactored into per‑boss files for better maintainability.

### Motivation
The previous removal was meant to avoid core modifications and unblock
upstreaming.
This PR brings the strategies back while adhering to that requirement,
using only observable state and mod‑playerbots helpers.

### What’s included

- Re‑enabled the Naxxramas strategies previously removed.
- Replaced core script header dependencies with observable checks
(auras, casts, unit flags, flight state, etc.).
- Split the Naxxramas action logic into per‑boss source files to avoid a
“god file” and ease future maintenance.
- Minor, non‑intrusive behavior improvements aligned with existing
helpers.

### Future work
Some strategies may still require refinement or more advanced handling
later.
This PR focuses on restoring the baseline logic without core
dependencies, while keeping changes minimal and safe.

**Any contributions are welcome to further improve and fine‑tune the
Naxxramas strategies.**

### Testing
Tested in some Naxx boxx.
No server crash and boss killed :D

Note: I'll make another PR with revised scripts when this one are merged

---------

Co-authored-by: Keleborn <22352763+Celandriel@users.noreply.github.com>
Co-authored-by: bash <hermensb@gmail.com>
Co-authored-by: Revision <tkn963@gmail.com>
Co-authored-by: kadeshar <kadeshar@gmail.com>

src/Ai/Raid/Naxxramas/Action/RaidNaxxActions_Razuvious.cpp
@@ -1,0 +1,147 @@
+#include "RaidNaxxActions.h"
+
+#include "ObjectGuid.h"
+#include "PlayerbotAIConfig.h"
+#include "Playerbots.h"
+#include "SharedDefines.h"
+
+bool RazuviousUseObedienceCrystalAction::Execute(Event /*event*/)
+{
+    if (!helper.UpdateBossAI())
+        return false;
+
+    // bot->GetCharm
+    if (Unit* charm = bot->GetCharm())
+    {
+        Unit* target = AI_VALUE2(Unit*, "find target", "instructor razuvious");
+        if (!target)
+            return false;
+
+        if (charm->GetMotionMaster()->GetMotionSlotType(MOTION_SLOT_ACTIVE) == NULL_MOTION_TYPE)
+        {
+            charm->GetMotionMaster()->Clear();
+            charm->GetMotionMaster()->MoveChase(target);
+            charm->GetAI()->AttackStart(target);
+        }
+        Aura* forceObedience = botAI->GetAura("force obedience", charm);
+        uint32 duration_time;
+        if (!forceObedience)
+        {
+            forceObedience = botAI->GetAura("mind control", charm);
+            duration_time = 60000;
+        }
+        else
+            duration_time = 90000;
+
+        if (!forceObedience)
+            return false;
+
+        if (charm->GetDistance(target) <= 0.51f)
+        {
+            // taunt
+            bool tauntUseful = true;
+            if (forceObedience->GetDuration() <= (duration_time - 5000))
+            {
+                if (target->GetVictim() && botAI->HasAura(29061, target->GetVictim()))
+                    tauntUseful = false;
+
+                if (forceObedience->GetDuration() <= 3000)
+                    tauntUseful = false;
+
+            }
+            if (forceObedience->GetDuration() >= (duration_time - 500))
+                tauntUseful = false;
+
+            if (tauntUseful && !charm->HasSpellCooldown(29060))
+            {
+                // shield
+                if (!charm->HasSpellCooldown(29061))
+                {
+                    charm->CastSpell(charm, 29061, true);
+                    charm->AddSpellCooldown(29061, 0, 30 * 1000);
+                }
+                charm->CastSpell(target, 29060, true);
+                charm->AddSpellCooldown(29060, 0, 20 * 1000);
+            }
+            // strike
+            if (!charm->HasSpellCooldown(61696))
+            {
+                charm->CastSpell(target, 61696, true);
+                charm->AddSpellCooldown(61696, 0, 4 * 1000);
+            }
+        }
+    }
+    else
+    {
+        Difficulty diff = bot->GetRaidDifficulty();
+        if (diff == RAID_DIFFICULTY_10MAN_NORMAL)
+        {
+            GuidVector npcs = AI_VALUE(GuidVector, "nearest npcs");
+            for (auto i = npcs.begin(); i != npcs.end(); i++)
+            {
+                Creature* unit = botAI->GetCreature(*i);
+                if (!unit)
+                    continue;
+
+                if (botAI->IsMainTank(bot) && unit->GetSpawnId() != 128352)
+                    continue;
+
+                if (!botAI->IsMainTank(bot) && unit->GetSpawnId() != 128353)
+                    continue;
+
+                if (MoveTo(unit, 0.0f, MovementPriority::MOVEMENT_COMBAT))
+                    return true;
+
+                Creature* creature = bot->GetNPCIfCanInteractWith(*i, UNIT_NPC_FLAG_SPELLCLICK);
+                if (!creature)
+                    continue;
+
+                creature->HandleSpellClick(bot);
+                return true;
+            }
+        }
+        else
+        {
+            GuidVector attackers = context->GetValue<GuidVector>("attackers")->Get();
+            Unit* target = nullptr;
+            for (auto i = attackers.begin(); i != attackers.end(); ++i)
+            {
+                Unit* unit = botAI->GetUnit(*i);
+                if (!unit)
+                    continue;
+                if (botAI->EqualLowercaseName(unit->GetName(), "death knight understudy"))
+                {
+                    target = unit;
+                    break;
+                }
+            }
+            if (target)
+            {
+                if (bot->GetDistance2d(target) > sPlayerbotAIConfig.spellDistance)
+                    return MoveNear(target, sPlayerbotAIConfig.spellDistance, MovementPriority::MOVEMENT_COMBAT);
+                else
+                    return botAI->CastSpell("mind control", target);
+            }
+        }
+    }
+    return false;
+}
+
+bool RazuviousTargetAction::Execute(Event /*event*/)
+{
+    if (!helper.UpdateBossAI())
+        return false;
+
+    Unit* razuvious = AI_VALUE2(Unit*, "find target", "instructor razuvious");
+    Unit* understudy = AI_VALUE2(Unit*, "find target", "death knight understudy");
+    Unit* target = nullptr;
+    if (botAI->IsTank(bot))
+        target = understudy;
+    else
+        target = razuvious;
+
+    if (AI_VALUE(Unit*, "current target") == target)
+        return false;
+
+    return Attack(target);
+}

ShatteredDawn/mod-playerbots

src/Ai/Raid/Naxxramas/Action/RaidNaxxActions_Razuvious.cpp
@@ -1,0 +1,147 @@
+#include "RaidNaxxActions.h"
+
+#include "ObjectGuid.h"
+#include "PlayerbotAIConfig.h"
+#include "Playerbots.h"
+#include "UnitAI.h"
+
+bool RazuviousUseObedienceCrystalAction::Execute(Event /*event*/)
+{
+    if (!helper.UpdateBossAI())
+        return false;
+
+    // bot->GetCharm
+    if (Unit* charm = bot->GetCharm())
+    {
+        Unit* target = AI_VALUE2(Unit*, "find target", "instructor razuvious");
+        if (!target)
+            return false;
+
+        if (charm->GetMotionMaster()->GetMotionSlotType(MOTION_SLOT_ACTIVE) == NULL_MOTION_TYPE)
+        {
+            charm->GetMotionMaster()->Clear();
+            charm->GetMotionMaster()->MoveChase(target);
+            charm->GetAI()->AttackStart(target);
+        }
+        Aura* forceObedience = botAI->GetAura("force obedience", charm);
+        uint32 duration_time;
+        if (!forceObedience)
+        {
+            forceObedience = botAI->GetAura("mind control", charm);
+            duration_time = 60000;
+        }
+        else
+            duration_time = 90000;
+
+        if (!forceObedience)
+            return false;
+
+        if (charm->GetDistance(target) <= 0.51f)
+        {
+            // taunt
+            bool tauntUseful = true;
+            if (forceObedience->GetDuration() <= (duration_time - 5000))
+            {
+                if (target->GetVictim() && botAI->HasAura(29061, target->GetVictim()))
+                    tauntUseful = false;
+
+                if (forceObedience->GetDuration() <= 3000)
+                    tauntUseful = false;
+
+            }
+            if (forceObedience->GetDuration() >= (duration_time - 500))
+                tauntUseful = false;
+
+            if (tauntUseful && !charm->HasSpellCooldown(29060))
+            {
+                // shield
+                if (!charm->HasSpellCooldown(29061))
+                {
+                    charm->CastSpell(charm, 29061, true);
+                    charm->AddSpellCooldown(29061, 0, 30 * 1000);
+                }
+                charm->CastSpell(target, 29060, true);
+                charm->AddSpellCooldown(29060, 0, 20 * 1000);
+            }
+            // strike
+            if (!charm->HasSpellCooldown(61696))
+            {
+                charm->CastSpell(target, 61696, true);
+                charm->AddSpellCooldown(61696, 0, 4 * 1000);
+            }
+        }
+    }
+    else
+    {
+        Difficulty diff = bot->GetRaidDifficulty();
+        if (diff == RAID_DIFFICULTY_10MAN_NORMAL)
+        {
+            GuidVector npcs = AI_VALUE(GuidVector, "nearest npcs");
+            for (auto i = npcs.begin(); i != npcs.end(); i++)
+            {
+                Creature* unit = botAI->GetCreature(*i);
+                if (!unit)
+                    continue;
+
+                if (botAI->IsMainTank(bot) && unit->GetSpawnId() != 128352)
+                    continue;
+
+                if (!botAI->IsMainTank(bot) && unit->GetSpawnId() != 128353)
+                    continue;
+
+                if (MoveTo(unit, 0.0f, MovementPriority::MOVEMENT_COMBAT))
+                    return true;
+
+                Creature* creature = bot->GetNPCIfCanInteractWith(*i, UNIT_NPC_FLAG_SPELLCLICK);
+                if (!creature)
+                    continue;
+
+                creature->HandleSpellClick(bot);
+                return true;
+            }
+        }
+        else
+        {
+            GuidVector attackers = context->GetValue<GuidVector>("attackers")->Get();
+            Unit* target = nullptr;
+            for (auto i = attackers.begin(); i != attackers.end(); ++i)
+            {
+                Unit* unit = botAI->GetUnit(*i);
+                if (!unit)
+                    continue;
+                if (botAI->EqualLowercaseName(unit->GetName(), "death knight understudy"))
+                {
+                    target = unit;
+                    break;
+                }
+            }
+            if (target)
+            {
+                if (bot->GetDistance2d(target) > sPlayerbotAIConfig.spellDistance)
+                    return MoveNear(target, sPlayerbotAIConfig.spellDistance, MovementPriority::MOVEMENT_COMBAT);
+                else
+                    return botAI->CastSpell("mind control", target);
+            }
+        }
+    }
+    return false;
+}
+
+bool RazuviousTargetAction::Execute(Event /*event*/)
+{
+    if (!helper.UpdateBossAI())
+        return false;
+
+    Unit* razuvious = AI_VALUE2(Unit*, "find target", "instructor razuvious");
+    Unit* understudy = AI_VALUE2(Unit*, "find target", "death knight understudy");
+    Unit* target = nullptr;
+    if (botAI->IsTank(bot))
+        target = understudy;
+    else
+        target = razuvious;
+
+    if (AI_VALUE(Unit*, "current target") == target)
+        return false;
+
+    return Attack(target);
+}