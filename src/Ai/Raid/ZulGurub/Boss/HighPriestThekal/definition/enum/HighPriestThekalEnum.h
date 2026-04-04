d6ee341d6ca98afb66880448298f97689ef85e38
Nicolas Lebacq <nicolas.cordier@outlook.com>
2026-04-04 21:00:34 +0200

    fix: Lowered Thekal and adds health difference from 5000 to 2500 for a smoother experience.

--- src/Ai/Raid/ZulGurub/Boss/HighPriestThekal/definition/enum/HighPriestThekalEnum.h
+++ src/Ai/Raid/ZulGurub/Boss/HighPriestThekal/definition/enum/HighPriestThekalEnum.h
@@ -7,5 +7,5 @@ enum class HighPriestThekalEnum : uint32_t
     ENTRY = 14509u,
     GUID = 49310u,
     SPELL_TIGER_FORM = 24169u,
-    MAXIMUM_HEALTH_DIFFERENCE = 5000u,
+    MAXIMUM_HEALTH_DIFFERENCE = 2500u,
 };
