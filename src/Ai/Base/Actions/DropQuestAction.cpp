8c3120c5dbaf3a7be9a432b448543437623a1913
Nicolas Lebacq <nicolas.cordier@outlook.com>
2026-05-09 00:54:40 +0200

    chore: Removed some unwanted changes from the original PR.

--- src/Ai/Base/Actions/DropQuestAction.cpp
+++ src/Ai/Base/Actions/DropQuestAction.cpp
@@ -61,10 +61,12 @@ bool DropQuestAction::Execute(Event event)
 bool CleanQuestLogAction::Execute(Event event)
 {
     Player* requester = event.getOwner() ? event.getOwner() : GetMaster();
-    if (!requester)
+    if (requester == nullptr)
+    {
         return false;
+    }
 
-    if (!sPlayerbotAIConfig.dropObsoleteQuests)
+    if (!PlayerbotAIConfig::instance().dropObsoleteQuests)
     {
         return false;
     }
